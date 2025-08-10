6bf94bda88cdd513f3326affdf7e9ac47cbe28ac
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-08-10 17:52:38 -0600

    Clases VisitorArrayList y FrmVisitors
    Se creo la clase VisitorArrayList con sus listas utilizando ArrayList y la clase FrmVisitor con su respectiva GUI

---
+++ src/Persons/Visitors/VisitorArrayList.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persons.Visitors;
+
+import Animals.Animal;
+import Lists.List;
+import Persons.Visitors.Visitor;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Eduard Salas Murillo
+ */
+public class VisitorArrayList implements List<Visitor>{
+   private ArrayList<Visitor>map;
+
+    public VisitorArrayList() {
+        this.map = new ArrayList<>();
+    }
+   
+    @Override
+    public boolean add(Visitor v) {
+        if (find(v.getId()) != null) {
+            return false; 
+        }
+        return this.map.add(v);
+    }
+
+    @Override
+    public boolean remove(Visitor v) {
+        Visitor visitorRemove = find(v.getId());
+        if (visitorRemove != null) {
+            return this.map.remove(visitorRemove);
+        }
+        return false; 
+    }
+
+    @Override
+    public Visitor find(Object id) {
+        String visitorId = String.valueOf(id);
+        for (Visitor visitor : this.map) {
+            if (visitor.getId().equals(visitorId)) {
+                return visitor; 
+            }
+        }
+        return null; 
+    }
+
+    @Override
+    public void showAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
