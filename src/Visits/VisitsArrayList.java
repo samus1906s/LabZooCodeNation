2c3da426676481f3c81d6f7e6f3785ebc88c2090
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-08-10 16:54:27 -0600

    Clase Visits
    Se añadieron los icons para la GUI, se creo el JFrame con todos los controles, etiquetas y cajas de texto, por ultimo se hizo la lista visitas con la estructura dinamica ArrayList

---
+++ src/Visits/VisitsArrayList.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Visits;
+import Lists.List;
+import Persons.Visitors.Visitor;
+import java.util.ArrayList;
+/**
+ *
+ * @author je110
+ */
+public class VisitsArrayList implements List<Visitor> {
+    ArrayList<Visitor> visitors;
+
+    public VisitsArrayList(ArrayList<Visitor> visitors) {
+        this.visitors = new ArrayList<>();
+    }
+
+    @Override
+    public boolean add(Visitor t) {
+        if (find(t.getId()) != null) return false;
+        visitors.add(t);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Visitor t) {
+        for (int i = 0; i < visitors.size(); i++) {
+            if (visitors.get(i).getId().equals(t.getId())) {
+                visitors.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Visitor find(Object id) {
+         String strId = String.valueOf(id);
+        for (Visitor visitor : visitors) {
+            if (visitor.getId().equals(strId)) {
+                return visitor;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void showAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
